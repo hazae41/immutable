8f0369f4ca51b8af9f15a4f9f3b84dd995a4a0ce
hazae41 <4405263+hazae41@users.noreply.github.com>
2024-07-28 22:36:04 +0700

    fix

--- src/mods/service_worker/index.ts
+++ src/mods/service_worker/index.ts
@@ -3,12 +3,18 @@ import { Nullable } from "@hazae41/option"
 import { Path } from "libs/path/index.js"
 import { JsonLocalStorage } from "libs/storage/index.js"
 
+export interface RegisterParams {
+  readonly shouldCheckUpdates?: boolean
+}
+
 /**
  * Register a sticky service-worker and return a function to update it
  * @param script 
  * @returns 
  */
-export async function register(script: string | URL): Promise<Nullable<() => Promise<void>>> {
+export async function register(script: string | URL, params: RegisterParams): Promise<Nullable<() => Promise<void>>> {
+  const { shouldCheckUpdates = true } = params
+
   const bricked = JsonLocalStorage.get("service_worker.bricked")
 
   if (bricked)
@@ -77,75 +83,81 @@ export async function register(script: string | URL): Promise<Nullable<() => Pro
   })
 
   const latestScriptUrl = new URL(script, location.href)
-  const latestScriptRes = await fetch(latestScriptUrl, { cache: "reload" })
-
-  if (!latestScriptRes.ok)
-    throw new Error(`Failed to fetch latest service-worker`)
-  if (latestScriptRes.headers.get("cache-control") !== "public, max-age=31536000, immutable")
-    throw new Error(`Wrong Cache-Control header for latest service-worker`)
 
   const [basename] = Path.filename(latestScriptUrl.pathname).split(".")
 
-  const latestHashBytes = new Uint8Array(await crypto.subtle.digest("SHA-256", await latestScriptRes.arrayBuffer()))
-  const latestHashRawHex = Array.from(latestHashBytes).map(b => b.toString(16).padStart(2, "0")).join("")
-  const latestVersion = latestHashRawHex.slice(0, 6)
-
-  const currentVersion = JsonLocalStorage.getOrSet("service_worker.current.version", latestVersion)
+  const currentVersion = JsonLocalStorage.get("service_worker.current.version")
   const currentVersionScriptPath = `${basename}.${currentVersion}.js`
   const currentVersionScriptUrl = new URL(currentVersionScriptPath, latestScriptUrl)
 
   await navigator.serviceWorker.register(currentVersionScriptUrl, { updateViaCache: "all" })
 
-  /**
-   * No update found
-   */
-  if (currentVersion === latestVersion)
+  if (!shouldCheckUpdates)
     return
 
-  return async () => {
-    const registration = await navigator.serviceWorker.getRegistration()
+  try {
+    const latestScriptRes = await fetch(latestScriptUrl, { cache: "reload" })
 
-    if (registration == null)
+    if (!latestScriptRes.ok)
+      throw new Error(`Failed to fetch latest service-worker`)
+    if (latestScriptRes.headers.get("cache-control") !== "public, max-age=31536000, immutable")
+      throw new Error(`Wrong Cache-Control header for latest service-worker`)
+
+    const latestHashBytes = new Uint8Array(await crypto.subtle.digest("SHA-256", await latestScriptRes.arrayBuffer()))
+    const latestHashRawHex = Array.from(latestHashBytes).map(b => b.toString(16).padStart(2, "0")).join("")
+    const latestVersion = latestHashRawHex.slice(0, 6)
+
+    if (latestVersion === currentVersion)
       return
 
-    const { active } = registration
+    return async () => {
+      const registration = await navigator.serviceWorker.getRegistration()
 
-    if (active == null)
-      return
-
-    const currentVersion = JsonLocalStorage.get("service_worker.current.version")
-
-    /**
-     * Recheck to avoid concurrent updates
-     */
-    if (currentVersion === latestVersion)
-      return
-
-    JsonLocalStorage.set("service_worker.current.version", latestVersion)
-    JsonLocalStorage.set("service_worker.pending.version", latestVersion)
-
-    const future = new Future<void>()
-
-    const onStateChange = async () => {
-      if (active.state !== "redundant")
+      if (registration == null)
         return
-      future.resolve()
-    }
 
-    try {
-      active.addEventListener("statechange", onStateChange, { passive: true })
+      const { active } = registration
 
-      const latestVersionScriptPath = `${basename}.${latestVersion}.js`
-      const latestVersionScriptUrl = new URL(latestVersionScriptPath, latestScriptUrl)
+      if (active == null)
+        return
 
-      await navigator.serviceWorker.register(latestVersionScriptUrl, { updateViaCache: "all" })
+      const currentVersion = JsonLocalStorage.get("service_worker.current.version")
 
       /**
-       * Wait for activation
+       * Recheck to avoid concurrent updates
        */
-      await future.promise
-    } finally {
-      active.removeEventListener("statechange", onStateChange)
+      if (currentVersion === latestVersion)
+        return
+
+      JsonLocalStorage.set("service_worker.current.version", latestVersion)
+      JsonLocalStorage.set("service_worker.pending.version", latestVersion)
+
+      const future = new Future<void>()
+
+      const onStateChange = async () => {
+        if (active.state !== "redundant")
+          return
+        future.resolve()
+      }
+
+      try {
+        active.addEventListener("statechange", onStateChange, { passive: true })
+
+        const latestVersionScriptPath = `${basename}.${latestVersion}.js`
+        const latestVersionScriptUrl = new URL(latestVersionScriptPath, latestScriptUrl)
+
+        await navigator.serviceWorker.register(latestVersionScriptUrl, { updateViaCache: "all" })
+
+        /**
+         * Wait for activation
+         */
+        await future.promise
+      } finally {
+        active.removeEventListener("statechange", onStateChange)
+      }
     }
+  } catch (e: unknown) {
+    console.warn(e)
+    return
   }
 }
